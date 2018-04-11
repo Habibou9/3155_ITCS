fb1adadbf7696c20078ca3790f4e5ebcb08b3ba9
habibou9 <hmaizoum@uncc.edu>
2018-04-11 17:22:52 +0000

    Create the new action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'address/index'
 
+resources :addresses
+
 root 'address#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
