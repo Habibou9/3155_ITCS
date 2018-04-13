e57c68ddb54c3c690edfea0b3782df10f1449b83
habibou9 <hmaizoum@uncc.edu>
2018-04-13 01:32:12 +0000

    Add edit/update functionnality

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -17,6 +17,18 @@ class AddressesController < ApplicationController
              render 'new'
        end
     end
+    def edit
+       @address = Address.find(params[:id])
+    end
+    
+  def update
+  @address = Address.find(params[:id])
+  if @address.update(address_params)
+    redirect_to @address
+   else
+    render 'edit'
+  end
+  end
 end
 
 private
