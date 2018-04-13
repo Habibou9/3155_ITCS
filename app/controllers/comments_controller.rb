b89174606ca9774528ebb830b9c797b552420a83
habibou9 <hmaizoum@uncc.edu>
2018-04-13 17:15:41 +0000

    Add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
     def create
-        @address = Address.find(params[:address_id])
+        @address = Address.find(params[address_id])
         @comment = @address.comments.create(comment_params)
         redirect_to address_path(@address)
     end
     
     private
-      def comment_params
+    def comment_params
         params.require(:comment).permit(:dateofbirth, :body)
-      end
+    end
 end
