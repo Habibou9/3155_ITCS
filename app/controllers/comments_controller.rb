ba30b144a1c658df064f831336f0f3d5c6b824b5
habibou9 <hmaizoum@uncc.edu>
2018-04-13 17:52:26 +0000

    Add comments to the show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
     def create
-        @address = Address.find(params[address_id])
+        @address = Address.find(params[:address_id])
         @comment = @address.comments.create(comment_params)
         redirect_to address_path(@address)
     end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
+    has_many :comments
     validates :FirstName, presence: true, length: {minimum: 3}
     validates :LastName, presence: true, length: {minimum: 3}
 end
