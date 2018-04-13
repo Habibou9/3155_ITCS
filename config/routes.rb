08d7e427412bf02bd0f7b2f1025c2cd7e7514858
habibou9 <hmaizoum@uncc.edu>
2018-04-13 17:08:31 +0000

    Add comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'address/index'
 
-resources :addresses
+resources :addresses do 
+  resources :comments
+end
 
 root 'address#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
