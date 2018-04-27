5ec9ca5bd5cb21e30a6510562bfca9a6e158cd33
Sandesh Khanal <skhanal@uncc.edu>
2018-04-27 15:10:07 +0000

    Add a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413170412) do
+ActiveRecord::Schema.define(version: 20180427150935) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "FirstName"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20180413170412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["address_id"], name: "index_comments_on_address_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
