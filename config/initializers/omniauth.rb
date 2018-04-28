03309adb8c8868d5b85dbcecd7aec9e9d5479c4f
prayamaj <prayamaj@uncc.edu>
2018-04-28 18:24:21 +0000

    Add omniauth.rb file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '996616246419-oohcs4gc8kc0998qbtm15qmg0n766u56.apps.googleusercontent.com', 'keON3zW_7yHDtBsU5YCxKwDX', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '1010168988200-6h36v1g885stgpjulhll6nfboku15b1m.apps.googleusercontent.com', 'GlhZoMmj8fhTw8cCW7iWqYvA', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
