5d8db1e859f7d08350699b496411be54692e9f4d
Sandesh Khanal <skhanal@uncc.edu>
2018-04-28 17:27:17 +0000

    Made CSS changes to tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'address/index'
-
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
+  
+  
 resources :addresses do 
   resources :comments
 end
